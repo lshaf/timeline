72a75f8710cd374c0d7d7341eda1241aac1f4d0b
L Shaf <shafry2008@gmail.com>
2021-04-03 23:26:28 +0700

    👩‍💻 complete essential form timeline.

--- assets/js/form.js
+++ assets/js/form.js
@@ -10,19 +10,14 @@ Vue.component('p-form', {
         let self = this;
         let defaultSetting = {
             offDay: [6, 0],
-            holiday: ["2021-04-02"],
+            holiday: ["2021-04-02", "2021-04-07", "2021-04-08", "2021-04-09"],
         };
 
-        let setup = Object.assign(defaultSetting, this.setting);
+        let setup = Object.assign(defaultSetting, this.$parent.setting);
 
         return {
-            setting: this.$parent.setting,
-            defaultSchedule: {
-                description: "",
-                startDate: this.dateFormatter(new Date()),
-                manDays: 1,
-                finishDate: ""
-            },
+            setting: setup,
+            activeKey: null,
             disabledDates: {
                 days: setup.offDay,
                 customPredictor (date) {
@@ -32,15 +27,52 @@ Vue.component('p-form', {
             },
         };
     },
+    computed: {
+        startingDate() {
+            let decDate = 0;
+            let current = new Date();
+            if (current.getDay() == 6) decDate = 1;
+            if (current.getDay() == 0) decDate = 2;
+
+            current.setDate(current.getDate() - decDate);
+            let fmtDate = this.dateFormatter(current);
+            while (this.setting.holiday.includes(fmtDate)) {
+                current.setDate(current.getDate() - 1);
+                fmtDate = this.dateFormatter(current);
+            }
+
+            return this.dateFormatter(current);
+        },
+        activeSchedule() {
+            activeSchedule = this.timeline.schedules[this.activeKey];
+            if (activeSchedule === undefined) {
+                activeSchedule = Object.assign({}, this.defaultSchedule);
+            }
+
+            return activeSchedule;
+        },
+        defaultSchedule() {
+            return {
+                description: "",
+                startDate: this.startingDate,
+                manDays: 1,
+                finishDate: ""
+            };
+        }
+    },
     created() {
-        console.log(this, this.timeline);
+        let p = this.dateFormatter(new Date()).replace("-", "");
+        let r = Math.ceil(Math.random() * Math.pow(10, 12));
+
         let defaultTimeline = {
-            id: "xxx",
+            id: p + String(r).padStart(12, "0"),
             name: "",
-            schedules: [this.defaultSchedule]
+            schedules: [
+                Object.assign({}, this.defaultSchedule)
+            ]
         };
+
         this.timeline = Object.assign(defaultTimeline, this.timeline);
-        console.log('after', this.timeline);
     },
     methods: {
         dateFormatter(tmpDate) {
@@ -52,22 +84,46 @@ Vue.component('p-form', {
             let tmpDate = new Date(date);
             return this.dateFormatter(tmpDate);
         },
+        manDaysCalculator(masterManDays, startDate) {
+            let manDays = masterManDays - 1;
+            let endDate = new Date(startDate);
+            let fmtEd = this.dateFormatter(endDate);
+        
+            while (manDays > 0) {
+                endDate.setDate(endDate.getDate() + 1);
+                manDays--;
+        
+                fmtEd = this.dateFormatter(endDate);
+                if (
+                    this.setting.holiday.includes(fmtEd) 
+                    || this.setting.offDay.includes(endDate.getDay())
+                ) {
+                    manDays++;
+                }
+            }
+        
+            return fmtEd;
+        },
         calculateEnd(scheduleKey) {
             let schedule = this.timeline.schedules[scheduleKey];
-            let tmpDate = new Date(schedule.startDate);
-            tmpDate.setDate(tmpDate.getDate() + schedule.manDays);
-            return this.dateFormatter(tmpDate);
+            return this.manDaysCalculator(schedule.manDays, schedule.startDate);
         },
         adjustDate(scheduleKey) {
-            console.log('adjust');
+            let totalSch = this.timeline.schedules.length;
+            let currentED = this.calculateEnd(scheduleKey);
+            for (lp = (scheduleKey+1);lp < totalSch;lp++) {
+                this.timeline.schedules[lp].startDate = this.manDaysCalculator(2, currentED);
+                currentED = this.calculateEnd(lp);
+            }
         },
         addSchedule() {
-            this.timeline.schedules.push(this.defaultSchedule);
+            this.timeline.schedules.push(Object.assign({}, this.defaultSchedule));
         },
         deleteSchedule(scheduleKey) {
             this.timeline.schedules.splice(scheduleKey, 1);
         },
         finishSchedule(scheduleKey, isFinish) {
+            console.log(scheduleKey);
             let finish = (isFinish == 1) ? this.dateFormatter(new Date()) : "";
             this.timeline.schedules[scheduleKey].finishDate = finish;
         },
@@ -78,7 +134,7 @@ Vue.component('p-form', {
             this.$parent.route = 'list';
         },
         save() {
-            console.log('saved')
+            console.log('saved', this.timeline.schedules);
         }
     }
 })
--- src/pages/form.php
+++ src/pages/form.php
@@ -1,50 +1,49 @@
 <template id="page-form">
-    <div class="form">
-        <div class="form-group">
-            <label for="title">Judul Timeline</label>
-            <input type="text" class="form-control" v-model="timeline.name">
-        </div>
-        <div class="form-timeline" v-for="(schedule, s_key) in timeline.schedules">
-            <div class="block-activity" :key="s_key">
-                <div class="main-set">
-                    <textarea class="form-control" :disabled="isFinish(s_key)"
-                        v-model="timeline.schedules[s_key].description"></textarea>
-                </div>
-                <div class="input-set">
-                    <input type="number" class="form-control man-days" :disabled="isFinish(s_key)"
-                        v-model="timeline.schedules[s_key].manDays">
-                    <datepicker :format="customFormat" :disabled-dates="disabledDates" 
-                        input-class="form-control start-date" :disabled="isFinish(s_key)"
-                        v-model="timeline.schedules[s_key].startDate"></datepicker>
-                    <input type="text" class="form-control end-date" :value="calculateEnd(s_key)" disabled>
-                </div>
-                <div class="action-set">
-                    <div v-if="!isFinish(s_key)">
-                        <button type="button" @click="deleteSchedule(s_key)" class="btn btn-danger"
-                            v-if="timeline.schedules.length > 1">Delete</button>
-                        <button type="button" @click="finishSchedule(s_key, 1)" class="btn btn-success"
-                            >Finish</button>
-                    </div>
-                    <div v-else>
-                        <span class="finish-label">{{ schedule.finishDate }}</span>
-                        <button type="button" @click="finishSchedule(s_key, 0)" class="btn btn-warning"
-                            >Unfinish</button>
-                    </div>
-                    <button type="button" @click="adjustDate(s_key)" class="btn btn-info"
-                        >Adjust Date</button>
-                    <button type="button" @click="addSchedule()"
-                        v-if="(s_key + 1) == timeline.schedules.length" class="btn btn-main"
-                        >New Schedule</button>
-                </div>
-            </div>
-        </div>
-        <div class="actions">
-            <button type="button" @click="back()" class="btn btn-big btn-warning">
-                Back
-            </button>
-            <button type="button" @click="save()" class="btn btn-big btn-main pull-right">
-                Save
-            </button>
-        </div>
+  <div class="form">
+    <div class="form-group">
+      <label for="title">Judul Timeline</label>
+      <input type="text" class="form-control" v-model="timeline.name">
     </div>
+    <div class="form-timeline" v-for="(schedule, s_key) in timeline.schedules">
+      <div class="block-activity" :key="s_key">
+        <div class="main-set">
+          <textarea class="form-control" :disabled="isFinish(s_key)"
+            v-model="schedule.description"></textarea>
+        </div>
+        <div class="input-set">
+          <input type="number" class="form-control man-days" :disabled="isFinish(s_key)"
+            v-model.number="schedule.manDays">
+          <datepicker :format="customFormat" :disabled-dates="disabledDates" 
+            input-class="form-control start-date" :disabled="isFinish(s_key)"
+            v-model="schedule.startDate"></datepicker>
+          <input type="text" class="form-control end-date" :value="calculateEnd(s_key)" disabled>
+        </div>
+        <div class="action-set">
+          <div v-if="!isFinish(s_key)">
+            <button type="button" @click="deleteSchedule(s_key)" class="btn btn-danger"
+              v-if="timeline.schedules.length > 1">Delete</button>
+            <button type="button" @click="finishSchedule(s_key, 1)" class="btn btn-success"
+              >Finish</button>
+          </div>
+          <div v-else>
+            <span class="finish-label">{{ schedule.finishDate }}</span>
+            <button type="button" @click="finishSchedule(s_key, 0)" class="btn btn-warning"
+              >Unfinish</button>
+          </div>
+          <button type="button" @click="adjustDate(s_key)" class="btn btn-info"
+            >Adjust Date</button>
+          <button type="button" @click="addSchedule()" class="btn btn-main"
+            v-if="(s_key + 1) == timeline.schedules.length">New Schedule</button>
+        </div>
+      </div>
+    </div>
+    <div class="actions">
+      <button type="button" @click="back()" class="btn btn-big btn-warning">
+        Back
+      </button>
+      <button type="button" @click="save()" class="btn btn-big btn-main pull-right">
+        Save
+      </button>
+    </div>
+  </div>
 </template>
